ba2f4be2f3baaa15cdef00709e229a81d76ccb0f
CotofanaV <plastmarian@gmail.com>
2022-05-30 12:48:45 +0300

    guess counter

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 
 import random
 import time
+from tkinter import Y
 
 def choose_interval():
     print("Choose a range of numbers in which you want to play.")
@@ -17,35 +18,42 @@ def choose_interval():
         b = n1
         return(a,b)
 
+
+
 def play():
     
     a,b = choose_interval()
     guess_number = random.randint(a,b)
     number = int(input("Select a number between " + str(a) + " and " + str(b) +"\n"))
+    guessing_count = 1
+    print("You tried: ", guessing_count)
     while True:
+
         if number != guess_number and number <= guess_number:
-            print("Choose a number greater than ",number)
+            print("Choose a number greater than ",number)            
             number = int(input())
         elif number != guess_number and number >= guess_number:
-            print("Choose a number smaller than ",number)
+            print("Choose a number smaller than ",number)           
             number = int(input())
+
         else:
             print("You guessed it!!")
             quit()
+        guessing_count += 1
+        print("You tried: ", guessing_count)
 
-
-decizie = input("Do you want to play? Choose y / n: ")
+decision = input("Do you want to play? Choose y / n: ")
 while True:
 
-    if decizie == "y":
+    if decision == "y":
         print("Let's get started!")
         play()
 
-    elif decizie == "n":
+    elif decision == "n":
         print("Maybe another time!")
         quit()
 
 
     else:
         print("Choose y sau n")
-        decizie = input()
+        decision = input()
